3c4055e3cb65cf45fa69f282d6cea6f63051fe0d
BerkayCimenn <berkaycimen28@gmail.com>
2025-10-10 22:52:27 +0300

    Update Tablolar.sql

--- database/Tablolar.sql
+++ database/Tablolar.sql
@@ -1,5 +1,5 @@
---Tablo Oluþturma
--- Kullanýcýlar Tablosu
+--Tablo Oluşturma
+-- Kullanıcılar Tablosu
 CREATE TABLE Kullanicilar (
     KullaniciID INT IDENTITY(1,1) PRIMARY KEY,
     AdSoyad NVARCHAR(100) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE Kullanicilar (
     UyeTipi NVARCHAR(20) DEFAULT 'standart' CHECK (UyeTipi IN ('standart','ogrenci','vip')),
     Rol NVARCHAR(20) DEFAULT 'uye' CHECK (Rol IN ('uye','gorevli','admin'))
 );
--- Kullanýcý Ödemeleri Tablosu
+-- Kullanıcı Ödemeleri Tablosu
 CREATE TABLE KullaniciOdemeleri (
     OdemeID INT IDENTITY(1,1) PRIMARY KEY,
     KullaniciID INT FOREIGN KEY REFERENCES Kullanicilar(KullaniciID),
@@ -27,17 +27,17 @@ CREATE TABLE Katlar (
 CREATE TABLE Odalar (
     OdaID INT IDENTITY(1,1) PRIMARY KEY,
     KatID INT FOREIGN KEY REFERENCES Katlar(KatID),
-    OdaTipi NVARCHAR(50),  -- VIP 2 kiþilik, VIP 3-4 kiþilik
+    OdaTipi NVARCHAR(50),  -- VIP 2 kişilik, VIP 3-4 kişilik
     Kapasite INT,
     Musait BIT DEFAULT 1
 );
--- Örnek kayýtlar
+-- Örnek kayıtlar
 INSERT INTO Katlar (KatID, Ad) VALUES 
 (1, 'Alt Kat - Sesli'), 
 (2, 'Üst Kat - Sessiz');
 
 -- Kitaplar
---kütüphanedeki kaynaklarýn temel bilgileri
+--kütüphanedeki kaynakların temel bilgileri
 CREATE TABLE Kitaplar (
     KitapID INT IDENTITY(1,1) PRIMARY KEY,
     Baslik NVARCHAR(200) NOT NULL,
@@ -45,17 +45,17 @@ CREATE TABLE Kitaplar (
     YayinYili INT,
     Durum NVARCHAR(20) DEFAULT 'Mevcut' CHECK (Durum IN ('Mevcut','OduncAlindi'))
 );
---ISBN sütunu eklendi
+--ISBN sütunu ekleme
 ALTER TABLE dbo.Kitaplar
 ADD ISBN NVARCHAR(20) NULL;
 
---Kitap Stok Sayýsý Ekleme
+--Kitap Stok Sayısı Ekleme
 ALTER TABLE Kitaplar
 ADD StokSayisi INT DEFAULT 0;
 
 
--- Ödünç kayýtlarý
---kim hangi kitabý aldý, iade durumu
+-- Ödünç kayıtları
+--kim hangi kitabı aldı, iade durumu
 CREATE TABLE OduncAlmalar (
     OduncID INT IDENTITY(1,1) PRIMARY KEY,
     KullaniciID INT FOREIGN KEY REFERENCES Kullanicilar(KullaniciID),
@@ -68,7 +68,7 @@ CREATE TABLE OduncAlmalar (
 CREATE TABLE KafeUrunleri (
     UrunID INT IDENTITY(1,1) PRIMARY KEY,
     Ad NVARCHAR(100) NOT NULL,
-    Tip NVARCHAR(50),  -- Kahve, Sandviç, Tatlý
+    Tip NVARCHAR(50),  -- Kahve, Sandviç, Tatlı
     Fiyat DECIMAL(10,2) NOT NULL
 );
 
@@ -86,7 +86,7 @@ CREATE TABLE MenuDetaylari (
     UrunID INT FOREIGN KEY REFERENCES KafeUrunleri(UrunID)
 );
 
--- Sipariþler
+-- Siparişler
 CREATE TABLE Siparisler (
     SiparisID INT IDENTITY(1,1) PRIMARY KEY,
     KullaniciID INT FOREIGN KEY REFERENCES Kullanicilar(KullaniciID),
@@ -94,7 +94,7 @@ CREATE TABLE Siparisler (
     Toplam DECIMAL(10,2)
 );
 
--- Sipariþ detaylarý
+-- Sipariş detayları
 CREATE TABLE SiparisDetaylari (
     DetayID INT IDENTITY(1,1) PRIMARY KEY,
     SiparisID INT FOREIGN KEY REFERENCES Siparisler(SiparisID),
@@ -112,3 +112,43 @@ JOIN Kullanicilar K ON K.KullaniciID = S.KullaniciID
 WHERE KU.Tip = 'Kahve'
 GROUP BY K.KullaniciID, K.AdSoyad
 HAVING COUNT(*) >= 10;
+
+--OdaRezervasyonlari
+CREATE TABLE OdaRezervasyonlari (
+    RezervasyonID INT IDENTITY(1,1) PRIMARY KEY,
+    OdaID INT NOT NULL,
+    KullaniciID INT NOT NULL,
+    BaslangicTarihi DATETIME NOT NULL,
+    BitisTarihi DATETIME NOT NULL,
+    Durum NVARCHAR(20) DEFAULT 'Aktif', -- Aktif, İptal, Tamamlandı
+    Aciklama NVARCHAR(255) NULL,
+    OlusturmaTarihi DATETIME DEFAULT GETDATE(),
+
+    CONSTRAINT FK_Rezervasyon_Oda FOREIGN KEY (OdaID) REFERENCES Odalar(OdaID),
+    CONSTRAINT FK_Rezervasyon_Kullanici FOREIGN KEY (KullaniciID) REFERENCES Kullanicilar(KullaniciID)
+);
+
+ CREATE TABLE dbo.KullaniciGirisleri (
+        ID INT IDENTITY(1,1) PRIMARY KEY,
+        KullaniciID INT NOT NULL,
+        Basarili BIT NOT NULL,
+        IPAdres NVARCHAR(50) NULL,
+        Tarih DATETIME NOT NULL DEFAULT GETDATE(),
+        FOREIGN KEY (KullaniciID) REFERENCES Kullanicilar(KullaniciID)
+    );
+
+
+--Günlük Kafe Satış Raporu
+CREATE TABLE KafeSatislar (
+    SatisID INT IDENTITY(1,1) PRIMARY KEY,
+    UrunID INT NOT NULL,
+    Miktar INT NOT NULL,
+    Toplam DECIMAL(10,2) NOT NULL,
+    SatisTarihi DATETIME NOT NULL DEFAULT GETDATE()
+);
+
+CREATE TABLE Urunler (
+    UrunID INT IDENTITY(1,1) PRIMARY KEY,
+    UrunAdi NVARCHAR(100) NOT NULL,
+    Fiyat DECIMAL(10,2) NOT NULL
+);
